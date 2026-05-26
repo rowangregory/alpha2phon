e19f35012320316f9c53eab35f927858192f96de
rowangregory <rpgregory0404@jefcoed.com>
2026-05-26 01:21:03 -0500

    Added a slight touch
    Added the stylistic Ø character, and allowed more arguments to be taken in

--- alpha2phon.py
+++ alpha2phon.py
@@ -3,10 +3,10 @@ phonetics = {"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo
              "I": "India", "J": "Juliet", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa",
              "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey",
              "X": "X-ray", "Y": "Yankee", "Z": "Zulu", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five",
-             "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", "10": "Ten"}
+             "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", "10": "Ten", "Ø": "Zero"}
 # Join the phonetics to the "output" variable
 output = ''
-for convertedLetter in sys.argv[1]:
+for convertedLetter in sys.argv[1:]:
     # print(convertedLetter)
     output += phonetics[convertedLetter.upper()] + " "
 print(output)
