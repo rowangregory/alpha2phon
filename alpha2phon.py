f4409be3d6b926c84240a976e01c7fe76be95f14
rowangregory <rpgregory0404@jefcoed.com>
2026-06-11 16:08:26 +0000

    Added option to display possible variants
    Do note that the names of these variants were taken from Wikipedia

--- alpha2phon.py
+++ alpha2phon.py
@@ -1,20 +1,36 @@
 import sys
-phonetics = {"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel",
+normal = {"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "Delta", "E": "Echo", "F": "Foxtrot", "G": "Golf", "H": "Hotel",
              "I": "India", "J": "Juliet", "K": "Kilo", "L": "Lima", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa",
              "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey",
              "X": "X-ray", "Y": "Yankee", "Z": "Zulu", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five",
              "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", "10": "Ten", "Ø": "Zero"}
-howToUse = '''alpha2phon is a terminal utility to turn characters into their NATO phonetic versions.
-usage: alpha2phon [ARGUMENTS]'''
-# Join each phoneticized argument to the "output" variable
-output = []
-if len(sys.argv) != 1: # Has the user provided more than one argument?
-    for argument in sys.argv[1:]:
+variant = {"A": "Alfa", "B": "Bravo", "C": "Charlie", "D": "[Delta, David, Dixie]", "E": "Echo", "F": "[Foxtrot, Fox]", "G": "Golf", "H": "[Hawk, Hotel]",
+             "I": "[India, Indigo]", "J": "Juliet", "K": "Kilo", "L": "[Lima, London]", "M": "Mike", "N": "November", "O": "Oscar", "P": "Papa",
+             "Q": "Quebec", "R": "Romeo", "S": "Sierra", "T": "Tango", "U": "Uniform", "V": "Victor", "W": "Whiskey",
+             "X": "X-ray", "Y": "Yankee", "Z": "Zulu", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five",
+             "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine", "10": "Ten", "Ø": "Zero"}
+def convert(type, stringLocation):
+    for argument in sys.argv[stringLocation:]:
         #Debug purposes: print(argument)
         for convertedLetter in argument:
             #Debug purposes: print(convertedLetter)
-            output.append(phonetics[convertedLetter.upper()])
-    # Display all the words in a normal manner, not everyone understands ["H", "E", ...]
+            output.append(type[convertedLetter.upper()])
+        # Display all the words in a normal manner, not everyone understands ["H", "E", ...]
     print(" ".join(output))
+howToUse = '''alpha2phon is a terminal utility to turn characters into their NATO phonetic versions.
+usage: alpha2phon [ -v ] [STRINGS]...
+
+Options:
+    -v			print out possible alternative variants to letters (i.e. Indigo instead of India, David instead of Delta, etc.)
+
+By default, alpha2phon acts as if there were only the strings given as arguments.'''
+# Join each phoneticized argument to the "output" variable
+output = []
+if len(sys.argv) != 1: # Has the user provided more than one argument?
+    match sys.argv[1]:
+        case "-v":
+            convert(variant, 2)
+        case _:
+            convert(normal, 1)
 else: # No? Alright then, display how it should be used.
     print(howToUse)
